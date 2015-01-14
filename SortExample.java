d0a6200c238dfaa452f7fa19a7a926d1c22a4e35
luisdvalle <luisdvalle@users.noreply.github.com>
2015-01-14 13:24:51 +1100

    Create SortExample.java

---
+++ SortExample.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class SortExample
+{
+	//Class to sort an array of integers
+	int[] arrayInt;
+	
+	SortExample(int[] arrayInt)
+	{
+		this.arrayInt = arrayInt;
+	}
+	
+	void sortingArray()
+	{
+		Arrays.sort(arrayInt);
+	}
+}
